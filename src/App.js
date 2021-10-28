3ff4a2f392e8cf1dbfab0eb994c65c98feb21226
Alson33 <garbuja12alson@gmial.com>
2021-10-28 21:31:17 +0545

    add layout and routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import { DetailPage, HomePage, Navbar } from './components';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <main>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" exact>
+              <HomePage />
+            </Route>
+            <Route path="/detail/:id/:selectedCategory">
+              <DetailPage />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </main>
     </div>
   );
 }
