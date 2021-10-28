10ed896fac5cbdf9a3dd17ea00c3bc10972c4f33
Alson33 <garbuja12alson@gmial.com>
2021-10-28 21:34:29 +0545

    add index for exporting

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Navbar } from './navbar/Navbar'
+export { default as HomePage } from './home/HomePage'
+export { default as MorePage } from './more/MorePage'
+export { default as DetailPage } from './detail/DetailPage'
