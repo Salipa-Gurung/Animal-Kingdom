df484d5a937652f39192d09b8398414b132ebefe
Alson33 <garbuja12alson@gmial.com>
2021-10-28 21:28:49 +0545

    add dummy data for testing UI

---
+++ src/utils/data.js
@@ -0,0 +1,58 @@
+export const data = [
+    {
+        id: '0',
+        name: 'Husky',
+        lifeSpan: '12 yrs',
+        description: 
+            `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, ea. Aut ullam modi ratione nulla recusandae quia eum non? Ipsum, quisquam fugiat? Impedit assumenda, necessitatibus qui voluptatum repudiandae pariatur ipsa!
+            Veritatis sapiente, nostrum facilis excepturi ratione dignissimos nam nemo culpa! Sunt consequatur eius aspernatur dignissimos rem, quod magni corrupti dolor reiciendis quam corporis officia quibusdam voluptates fugit repellat explicabo sit!`,
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWwM89h_vfaIJHKU7ut_S8TSCOvSjGkyfu3g&usqp=CAU'
+    },
+    {
+        id: '1',
+        name: 'Spitz',
+        lifeSpan: '12 yrs',
+        description: 
+            `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, ea. Aut ullam modi ratione nulla recusandae quia eum non? Ipsum, quisquam fugiat? Impedit assumenda, necessitatibus qui voluptatum repudiandae pariatur ipsa!
+            Veritatis sapiente, nostrum facilis excepturi ratione dignissimos nam nemo culpa! Sunt consequatur eius aspernatur dignissimos rem, quod magni corrupti dolor reiciendis quam corporis officia quibusdam voluptates fugit repellat explicabo sit!`,
+        url: 'https://images.unsplash.com/photo-1608764495572-dc0a36d0a8e7?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8c3BpdHp8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+        id: '2',
+        name: 'German Sheperd',
+        lifeSpan: '12 yrs',
+        description: 
+            `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, ea. Aut ullam modi ratione nulla recusandae quia eum non? Ipsum, quisquam fugiat? Impedit assumenda, necessitatibus qui voluptatum repudiandae pariatur ipsa!
+            Veritatis sapiente, nostrum facilis excepturi ratione dignissimos nam nemo culpa! Sunt consequatur eius aspernatur dignissimos rem, quod magni corrupti dolor reiciendis quam corporis officia quibusdam voluptates fugit repellat explicabo sit!`,
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGdcK0vYS081-gDEMg4D0yephgFCs6uNl1fA&usqp=CAU'
+    },
+    {
+        id: '3',
+        name: 'Pug',
+        lifeSpan: '12 yrs',
+        description: 
+        `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, ea. Aut ullam modi ratione nulla recusandae quia eum non? Ipsum, quisquam fugiat? Impedit assumenda, necessitatibus qui voluptatum repudiandae pariatur ipsa!
+        Veritatis sapiente, nostrum facilis excepturi ratione dignissimos nam nemo culpa! Sunt consequatur eius aspernatur dignissimos rem, quod magni corrupti dolor reiciendis quam corporis officia quibusdam voluptates fugit repellat explicabo sit!`,
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT77gtN63t37sGRxev1K0dvIFOxWr7q69qNsA&usqp=CAU'
+    },
+    {
+        id: '4',
+        name: 'Popperian',
+        lifeSpan: '12 yrs',
+        description: 
+        `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, ea. Aut ullam modi ratione nulla recusandae quia eum non? Ipsum, quisquam fugiat? Impedit assumenda, necessitatibus qui voluptatum repudiandae pariatur ipsa!
+        Veritatis sapiente, nostrum facilis excepturi ratione dignissimos nam nemo culpa! Sunt consequatur eius aspernatur dignissimos rem, quod magni corrupti dolor reiciendis quam corporis officia quibusdam voluptates fugit repellat explicabo sit!`,
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB6t_SbG1YZFc6X6gxLr7bU7YX7hiTVk31jw&usqp=CAU'
+    },
+    {
+        id: '5',
+        name: 'Bulldog',
+        lifeSpan: '8 yrs',
+        description:
+            `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, ea. Aut ullam modi ratione nulla recusandae quia eum non? Ipsum, quisquam fugiat? Impedit assumenda, necessitatibus qui voluptatum repudiandae pariatur ipsa!
+            Veritatis sapiente, nostrum facilis excepturi ratione dignissimos nam nemo culpa! Sunt consequatur eius aspernatur dignissimos rem, quod magni corrupti dolor reiciendis quam corporis officia quibusdam voluptates fugit repellat explicabo sit!`,
+        url: 'https://images.pexels.com/photos/9992763/pexels-photo-9992763.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+    },
+    
+];
+
